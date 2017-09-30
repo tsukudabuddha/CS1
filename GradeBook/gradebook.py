8621ddf1af133b66bf2515be4a3d782f9115e78f
tsukudabuddha <andrewtsukuda@gmail.com>
2017-09-29 22:33:06 -0700

    added list of classes and created dictionary to hold students

--- GradeBook/gradebook.py
+++ GradeBook/gradebook.py
@@ -9,9 +9,22 @@ class Classroom():
     Track all info required for class
     """
 
-    def __init__(self, name):
+    classes = []
+
+    def __init__(self, name, students):
         """Initialize the classroom with a name."""
         self.name = name
+        self.students = students
+        self.student_count = 0
+        self.student_dir = {}
+        for student in self.students:
+            self.student_dir[self.student_count] = student
+            self.student_count += 1
+        super().classes.append(self)  # Add classroom object to class list
+
+    def add_student(self, student):
+        """Add student to classroom."""
+        self.students.append(student)
 
 
 def runMenu():
@@ -28,3 +41,7 @@ def runMenu():
 def main():
     """Run main function of game."""
     command = runMenu()
+    if command == 1:
+        print("Which classroom would you like to add a student to?")
+        chosenClass = input("Your options are: ", Classroom.classes)
+        chose
